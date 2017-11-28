0f526869fbe35b5cadf5cefb5c27b91dc277ff38
Ruta Puodziunaite <ruta@rutas-mbp.ict.ad.dit.ie>
2017-11-28 18:30:10 +0000

    please work

--- codefirst/codefirst.py
+++ codefirst/codefirst.py
@@ -6,40 +6,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'codefirst.db'),
-    SECRET_KEY='codefirst girls',
-    USERNAME='flookie',
-    PASSWORD='flookie'
-))
-app.config.from_envvar('CODEFIRST_PROJECT_SETTINGS', silent=True)
-
-def connect_db():
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    init_db()
-    print('Initialised the database')
-
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-    
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
 @app.route('/')
 def show_entries():
     db = get_db()
@@ -77,3 +43,37 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+app.config.update(dict(
+                       DATABASE=os.path.join(app.root_path, 'codefirst.db'),
+                       SECRET_KEY='codefirst girls',
+                       USERNAME='flookie',
+                       PASSWORD='flookie'
+                       ))
+app.config.from_envvar('CODEFIRST_PROJECT_SETTINGS', silent=True)
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print('Initialised the database')
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
